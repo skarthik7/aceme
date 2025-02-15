a5c10e798e3841f76bcc3ae3d6110f1a2da8dd00
Sriram Karthik Akella <sriramka@ualberta.ca>
2025-02-15 11:37:34 -0700

    authentication done 😊

---
+++ lib/pages/login_register_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? errorMsg = "";
+  bool isLogin = true;
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMsg = e.message;
+      });
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMsg = e.message;
+      });
+    }
+  }
+
+  Widget _title() {
+    return Text('Ace Me');
+  }
+  
+  Widget _entryField(
+    String title,
+    TextEditingController controller,
+  ){
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
+      ),
+    );
+  }
+
+  Widget _errorMessage() {
+    return Text(errorMsg == ''? '': '$errorMsg');
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+      onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+      child: Text(isLogin ? 'Login' : 'Register'),
+    );
+  }
+
+  Widget _loginOrRegisterButton() {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          isLogin = !isLogin;
+        });
+      },
+      child: Text(isLogin ? 'Need an account? Register' : 'Already have an account? Login'),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(21),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _entryField('Email', _controllerEmail),
+            _entryField('Password', _controllerPassword),
+            _errorMessage(),
+            _submitButton(),
+            _loginOrRegisterButton(),
+          ],
+        ),
+      ),
+    );
+}
+}
---
+++ lib/widget_tree.dart
@@ -0,0 +1,24 @@
+import 'package:aceme/auth.dart';
+import 'package:aceme/pages/home_page.dart';
+import 'package:aceme/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget{
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree>{
+  @override
+  Widget build(BuildContext context){
+    return StreamBuilder(stream: Auth().authStateChanges, builder: (context, snapshot){
+      if (snapshot.hasData){
+        return HomePage();
+      } else {
+        return LoginPage();
+      }
+    });
+  }
+}
